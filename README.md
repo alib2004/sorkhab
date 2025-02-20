c2815d1cb98fbcc513eb3b764747d8e005804052
alib2004 <aliw.b2004@gmail.com>
2025-02-20 19:16:04 +0330

    structure & some library

---
+++ README.md
@@ -0,0 +1,4 @@
+data-aos="fade-up"
+data-aos-delay="800"
+data-aos-duration="500"
+data-aos-easing="ease-in-sine">n
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/about/About.jsx'
+function App() {
+  useEffect(()=>{
+    AOS.init()
+  },[])
+  
+  return (
+    <>
+      hi
+      <Routes>
+        <Route path='/' element={<h1>homes</h1>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
