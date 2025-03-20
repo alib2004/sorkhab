67587730d774c59777fc6d5f91819ec3ee35b9fb
alib2004 <aliw.b2004@gmail.com>
2025-03-20 17:32:41 +0330

    Update Cart.jsx

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import BreadCrunb from "../../components/breadcrumb/BreadCrunb";
 import { MdKeyboardArrowLeft } from "react-icons/md";
